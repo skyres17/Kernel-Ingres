27fbdb1402a06b927d6f5bcb8eacb6bdfe919dbd
Lseything <lseything@gmail.com>
2024-12-09 22:00:34 -0300

    ingres-kernel: Switch to Umbrella 6.1.94  * and disable extraction for kernel Signed-Off-by: Alex Lseything <lseything@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -12,7 +12,7 @@ DUMP=
 MY_DIR="${BASH_SOURCE%/*}"
 SRC_ROOT="${MY_DIR}/../../.."
 TMP_DIR=$(mktemp -d)
-EXTRACT_KERNEL=true
+EXTRACT_KERNEL=false
 declare -a MODULE_FOLDERS=("vendor_ramdisk" "vendor_dlkm")
 
 while [ "${#}" -gt 0 ]; do
